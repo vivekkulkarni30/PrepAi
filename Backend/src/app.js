435dbbf12d8204b37694e9ef5e96fce95dfb32f6
Vivek <your-email@gmail.com>
2026-03-29 19:15:15 +0530

    final fix for auth cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://prepai07.netlify.app"
+  ],
+  credentials: true
 }))
 
 /* require all the routes here */
